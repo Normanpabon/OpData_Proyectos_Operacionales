eb66d53e79f9197062125e3993dea2e68a25e7cd
Jose Armando Pagnusatt <japagnusatt@gmail.com>
2022-10-19 12:48:55 -0500

    Pequeños cambios
    Se borraron sobranted en UserMainPage y se agregó la fuente roboto a todo el proyecto

--- frontend/src/pages/user/UserMainPage.jsx
+++ frontend/src/pages/user/UserMainPage.jsx
@@ -1,35 +1,12 @@
-import { useEffect, useState } from "react";
 import ProjectList from "../../components/ProjectList";
-import ProjectForm from "../../components/ProjectForm";
 import { useUser } from "../../context/UserContext";
-import { useParams, Outlet } from "react-router-dom";
+import { Outlet } from "react-router-dom";
 
 function UserMainPage() {
   const { user } = useUser();
-  const [page, setPage] = useState("default");
-  const params = useParams();
-
-  useEffect(() => {
-    setPage(params);
-  }, []);
-
-  // var component = null;
-
-  // switch (page.estado) {
-  //   case "create":
-  //     component = <h1>Hola</h1>;
-  //     break;
-  //   case "edit":
-  //     component = <h1>Hola</h1>;
-  //   default:
-  //     component = <ProjectForm />;
-  //     break;
-  // }
-
   return (
-    <div>
+    <div className="">
       <h1>Bienvenido, {user.name}</h1>
-      {/* {component} */}
       <Outlet />
       <ProjectList />
     </div>

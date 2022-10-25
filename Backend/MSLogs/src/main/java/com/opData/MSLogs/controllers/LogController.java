e7ff8677f5860627a2c67cddb7d7f529bb467b07
Norman P <45250874+Normanpabon@users.noreply.github.com>
2022-10-25 12:15:20 -0500

    Esqueleto MsLogs
    cambios:
    - Se agrega el esqueleto para el microservicio encargado de los logs en el proyecto
    
    pendiente:
    - Implementar mapping para la creacion de logs
    - Conectar la api para que pueda hacer uso del ms de logs

---
+++ Backend/MSLogs/src/main/java/com/opData/MSLogs/controllers/LogController.java
@@ -0,0 +1,13 @@
+package com.opData.MSLogs.controllers;
+
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/MSLogs/V1/log/")
+@CrossOrigin
+public class LogController {
+    // Todo : Implementar metodos para crud de logs
+}

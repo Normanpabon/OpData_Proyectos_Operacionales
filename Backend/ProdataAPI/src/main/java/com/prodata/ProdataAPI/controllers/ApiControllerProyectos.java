81cda5160c777de7e776cb78ad7617874cf640e3
Norman P <45250874+Normanpabon@users.noreply.github.com>
2022-10-27 07:50:13 -0500

    fix
    cambios :
    
    - se crea un nuevo metodo post si se envia un proyecto nuevo sin descripcion.
    
    - Se retira la validacion de que la fecha de inicio sea mayor o igual a la fecha de inicio.

--- Backend/ProdataAPI/src/main/java/com/prodata/ProdataAPI/controllers/ApiControllerProyectos.java
+++ Backend/ProdataAPI/src/main/java/com/prodata/ProdataAPI/controllers/ApiControllerProyectos.java
@@ -129,6 +129,52 @@ public class ApiControllerProyectos {
 
     }
 
+
+    // Metodo post si no se envia observacion
+    @PostMapping("/{unidad}/{feReg}/{feIni}/{feEnd}/{desc}/{id_estado}")
+    @ResponseStatus(HttpStatus.CREATED)
+    @Validated
+    public Mono<Proyecto> createProyecto(@PathVariable @Positive(message = "El id debe ser mayor a 0 ") @NotNull(message = "La unidad asociada es obligatoria.") int unidad,
+                                         @PathVariable @NotBlank(message = "La fecha de registro es obligatoria.") String feReg,
+                                         @PathVariable @NotBlank(message = "La fecha de inicio es obligatoria.") String feIni,
+                                         @PathVariable @NotBlank(message = "La fecha de fin es obligatoria.")  String feEnd,
+                                         @PathVariable @NotBlank(message = "El nombre del proyecto es obligatorio.")  @Length(min=2, max=500, message = "La longitud maxima del nombre son 500 caracteres.") String desc,
+                                         @PathVariable @Positive(message = "El id debe ser mayor a 0 ") @NotNull(message = "El id de estado es obligatorio.") int id_estado){
+
+        // TODO: Abstraer a un validador externo
+
+        // si la primera fecha es mayor devuelve 0 > 1, si es igual 0
+        // Validar que la fecha de inicio sea mayor o igual a la de registro
+/*
+        if (!((LocalDate.parse(feIni, DATEFORMATTER)).compareTo(LocalDate.parse(feReg, DATEFORMATTER)) >= 0)) {
+
+            throw new ConstraintViolationException("La fecha de inicio debe ser mayor a la fecha de registro.", null);
+        }*/
+
+        // Validar que la fecha de fin sea mayor o igual a la de inicio
+        if(!((LocalDate.parse(feEnd, DATEFORMATTER)).compareTo(LocalDate.parse(feIni, DATEFORMATTER)) >= 0)){
+            throw new ConstraintViolationException("La fecha de finalizacion debe ser mayor a la fecha de registro e inicio.", null);
+        }
+
+        // Todo : Validar que la unidad exista
+        // Todo : Validar que el estado exista
+
+        // Validar que si el estado es anulado tenga una observacion
+        // todo : cambiar logica
+        if (id_estado == 4) {
+            throw new ConstraintViolationException("Para el estado anulado, se debe especificar la justificacion", null);
+        }
+
+
+        // Si se cumple la validacion de las fechas, se crea el proyecto
+
+        return proyectosServiceClient.addProyecto(unidad, feReg, feIni, feEnd, desc, id_estado, "NA");
+
+
+
+
+    }
+
     @PostMapping("/estado/{est}")
     @ResponseStatus(HttpStatus.CREATED)
     public void createEstado(@PathVariable  @NotBlank(message = "El estado debe tener un nombre.") String est){
